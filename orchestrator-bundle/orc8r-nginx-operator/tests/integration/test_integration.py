b99ea05e2aa1dc08037be9eb9cdc56ca836d722b
guillaume <guillaume.belanger27@gmail.com>
2021-12-17 15:22:14 -0500

    Bug fixes

--- orchestrator-bundle/orc8r-nginx-operator/tests/integration/test_integration.py
+++ orchestrator-bundle/orc8r-nginx-operator/tests/integration/test_integration.py
@@ -15,10 +15,10 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 CONTROLLER_METADATA = yaml.safe_load(
     Path("../orc8r-controller-operator/metadata.yaml").read_text()
 )
-APPLICATION_NAME = "orc8r-nginx"
-CHARM_NAME = "magma-orc8r-nginx-lite"
+NGINX_APPLICATION_NAME = "orc8r-nginx"
+NGINX_IMAGE_NAME = "magma-orc8r-nginx-lite-image"
 CONTROLLER_APPLICATION_NAME = "orc8r-controller"
-CONTROLLER_CHARM_NAME = "magma-orc8r-controller-lite"
+CONTROLLER_IMAGE_NAME = "magma-orc8r-controller-image"
 
 
 class TestOrc8rNginx:
@@ -32,15 +32,17 @@ class TestOrc8rNginx:
     async def test_build_and_deploy(self, ops_test, setup):
         charm = await ops_test.build_charm(".")
         resources = {
-            f"{CHARM_NAME}-image": METADATA["resources"][f"{CHARM_NAME}-image"]["upstream-source"],
+            NGINX_IMAGE_NAME: METADATA["resources"][NGINX_IMAGE_NAME]["upstream-source"],
         }
         await ops_test.model.deploy(
-            charm, resources=resources, application_name=APPLICATION_NAME, trust=True
+            charm, resources=resources, application_name=NGINX_APPLICATION_NAME, trust=True
         )
         await ops_test.model.add_relation(
-            relation1=APPLICATION_NAME, relation2="orc8r-controller:controller"
+            relation1=NGINX_APPLICATION_NAME, relation2="orc8r-controller:controller"
         )
-        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
+        await ops_test.model.wait_for_idle(
+            apps=[NGINX_APPLICATION_NAME], status="active", timeout=1000
+        )
 
     @staticmethod
     async def _deploy_postgresql(ops_test):
@@ -51,9 +53,9 @@ class TestOrc8rNginx:
     async def _deploy_orc8r_controller(ops_test):
         charm = await ops_test.build_charm("../orc8r-controller-operator/")
         resources = {
-            f"{CONTROLLER_CHARM_NAME}-image": CONTROLLER_METADATA["resources"][
-                f"{CONTROLLER_CHARM_NAME}-image"
-            ]["upstream-source"],
+            CONTROLLER_IMAGE_NAME: CONTROLLER_METADATA["resources"][CONTROLLER_IMAGE_NAME][
+                "upstream-source"
+            ],
         }
         await ops_test.model.deploy(
             charm,
